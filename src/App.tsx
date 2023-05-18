257d78fd7fb4769ad8475db5fed81270ce2feba8
Guilherme Turri <guilhermeturri@icloud.com>
2023-05-18 16:43:16 -0300

    feat[NotFound]:add a notFound route/template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { ProtectedRoute } from './routes/ProtectedRoute';
+import { NotFound } from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<ProtectedRoute />} />
+        <Route path="*" element={<NotFound />} />
+
       </Routes>
     </BrowserRouter>
 
---
+++ src/UI/NotFoundLocation/index.tsx
@@ -0,0 +1,10 @@
+import { Map } from '@styled-icons/foundation/Map'
+import * as Styled from './styles'
+
+export const NotFoundLocation = () => {
+  return (
+    <Styled.Img data-testid='NotFoundLocation'>
+      <Map />
+    </Styled.Img>
+  )
+}
